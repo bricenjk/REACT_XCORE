e247b97cc5e44429648199e1ed79c1f883d074c2
_NJKTech_ <brice.njomkam@gmail.com>
2023-03-01 10:39:59 +0100

    "feat: routes"

--- xcore/src/App.tsx
+++ xcore/src/App.tsx
@@ -1,22 +1,19 @@
-import { useState } from 'react'
-import './App.css'
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Home from './Components/Home/Home';
+import Login from './Components/User/LoginForm/LoginForm';
+import Register from './Components/User/RegisterForm/RegisterForm';
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <div className="App">
-     <div className="bg-white dark:bg-slate-800 rounded-lg px-6 py-8 ring-1 ring-slate-900/5 shadow-xl">
-  <div>
-    <span className="inline-flex items-center justify-center p-2 bg-indigo-500 rounded-md shadow-lg">
-    </span>
-  </div>
-  <p className="text-slate-500 dark:text-slate-400 mt-2 text-sm">
-    Hello World
-  </p>
-</div>
-    </div>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
