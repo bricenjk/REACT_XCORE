08296603b4a8387403d23c36732809de5fd5a7e5
_NJKTech_ <brice.njomkam@gmail.com>
2023-03-03 13:01:23 +0100

    "feat: better design"

--- xcore/src/components/User/RegisterForm/RegisterForm.tsx
+++ xcore/src/components/User/RegisterForm/RegisterForm.tsx
@@ -35,7 +35,7 @@ const RegisterForm = () => {
         setUserEmail(''),
         setUserPassword(''),
         setUserName('')
-        setTimeout(() => {setSuccessMessage; navigate('/login')}, 4000)
+        setTimeout(() => {setSuccessMessage; navigate('/')}, 4000)
 
       }).catch((error) => {setErrorMessage(error.message)});
     
@@ -50,17 +50,17 @@ const RegisterForm = () => {
     <div>
       <Navbar/>
       <div>
-      <section className="bg-gray-50 dark:bg-gray-900">
+      <section className="bg-gradient-to-l from-slate-900">
         <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
           <p
             className="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white"
           >
-             ⚡️ REGISTER 
+             🐣 REGISTER 
              <br/>
              {successMessage}
              {errorMessage}
           </p>
-          <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
+          <div className="w-full bg-white rounded-lg shadow md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
             <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
               <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
                 Welcome 
