03707a581fc2fda4837420f60f22c81d76d55019
_NJKTech_ <brice.njomkam@gmail.com>
2023-03-05 21:48:06 +0100

    fix( ): import fix

--- xcore/src/App.tsx
+++ xcore/src/App.tsx
@@ -1,9 +1,9 @@
+import './App.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Home from '../src/Components/Home/Home';
+import Home from './Components/Home/Home';
 import Login from './Components/User/LoginForm/LoginForm';
 import Cart from './Components/Cart/Cart';
 import Profile from './Components/User/Profile';
-import './App.css';
 import RegisterForm from './Components/User/RegisterForm/RegisterForm';
 
 function App() {
--- xcore/src/components/Cart/Cart.tsx
+++ xcore/src/components/Cart/Cart.tsx
@@ -1,35 +1,7 @@
-import Navbar from '../Navigation/Navbar';
-
-// interface CartProps {
-//   items: {
-//     id: number;
-//     name: string;
-//     description: string;
-//     price: number;
-//   }[];
-// }
-
-// const Cart: React.FC<CartProps> = ({ items }) => {
-//   return (
-//     <div>
-//       <h2>Cart</h2>
-//       {items.length === 0 && <p>Your cart is empty.</p>}
-//       {items.map((item) => (
-//         <ProductItem key={item.id} name={item.name} description={item.description} price={item.price} />
-//       ))}
-//     </div>
-//   );
-// };
-
-// export default Cart;
-
-
-
-function Cart() {
+const Cart = () => {
   return (
     <div>
-      <Navbar/>
-      Cart
+      
     </div>
   )
 }
